319b31684a25c3d8364884b4c56ae4f773d64698
Saheed muhammed <saheedm138@gmail.com>
2025-04-23 21:07:52 +0530

    Added sum of Two Number

---
+++ Challenge-5/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+   
+    while(b){
+      let temp=a
+      a=temp^b
+      b=(temp&b)<<1 
+    }
+    return a
+ 
+    
+};
